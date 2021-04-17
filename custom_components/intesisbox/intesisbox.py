aec819192d242bed3d7b6e771ec66bb7fc6e38ec
James <james@nimmo.net.nz>
2021-04-18 08:38:53 +1200

    Fix invalid import path

--- custom_components/intesisbox/intesisbox.py
+++ custom_components/intesisbox/intesisbox.py
@@ -8,37 +8,39 @@ from optparse import OptionParser
 from asyncio import ensure_future
 from time import sleep
 
-_LOGGER = logging.getLogger('pyintesisbox')
+_LOGGER = logging.getLogger("pyintesisbox")
 
 API_DISCONNECTED = "Disconnected"
 API_CONNECTING = "Connecting"
 API_AUTHENTICATED = "Connected"
 
-POWER_ON = 'ON'
-POWER_OFF = 'OFF'
+POWER_ON = "ON"
+POWER_OFF = "OFF"
 POWER_STATES = [POWER_ON, POWER_OFF]
 
-MODE_AUTO = 'AUTO'
-MODE_DRY = 'DRY'
-MODE_FAN = 'FAN'
-MODE_COOL = 'COOL'
-MODE_HEAT = 'HEAT'
+MODE_AUTO = "AUTO"
+MODE_DRY = "DRY"
+MODE_FAN = "FAN"
+MODE_COOL = "COOL"
+MODE_HEAT = "HEAT"
 MODES = [MODE_AUTO, MODE_DRY, MODE_FAN, MODE_COOL, MODE_HEAT]
 
-FUNCTION_ONOFF = 'ONOFF'
-FUNCTION_MODE = 'MODE'
-FUNCTION_SETPOINT = 'SETPTEMP'
-FUNCTION_FANSP = 'FANSP'
-FUNCTION_VANEUD = 'VANEUD'
-FUNCTION_VANELR = 'VANELR'
-FUNCTION_AMBTEMP = 'AMBTEMP'
-FUNCTION_ERRSTATUS = 'ERRSTATUS'
-FUNCTION_ERRCODE = 'ERRCODE'
+FUNCTION_ONOFF = "ONOFF"
+FUNCTION_MODE = "MODE"
+FUNCTION_SETPOINT = "SETPTEMP"
+FUNCTION_FANSP = "FANSP"
+FUNCTION_VANEUD = "VANEUD"
+FUNCTION_VANELR = "VANELR"
+FUNCTION_AMBTEMP = "AMBTEMP"
+FUNCTION_ERRSTATUS = "ERRSTATUS"
+FUNCTION_ERRCODE = "ERRCODE"
 
-NULL_VALUE = '32768'
+NULL_VALUE = "32768"
+
 
 class IHConnectionError(Exception):
     pass
+
 
 class IntesisBox(asyncio.Protocol):
     def __init__(self, ip, port=3310, loop=None):
@@ -83,7 +85,12 @@ class IntesisBox(asyncio.Protocol):
                     break
                 message = data.decode("ascii")
                 await self.data_received(message)
-            except (asyncio.IncompleteReadError, TimeoutError, ConnectionResetError, OSError) as e:
+            except (
+                asyncio.IncompleteReadError,
+                TimeoutError,
+                ConnectionResetError,
+                OSError,
+            ) as e:
                 _LOGGER.error(
                     "Lost connection to Intesisbox %s. Exception: %s", self._ip, e
                 )
@@ -98,24 +105,24 @@ class IntesisBox(asyncio.Protocol):
         _LOGGER.debug("Data received: {}".format(decoded_data))
         linesReceived = decoded_data.splitlines()
         for line in linesReceived:
-            cmdList = line.split(':', 1)
+            cmdList = line.split(":", 1)
             cmd = cmdList[0]
             args = None
             if len(cmdList) > 1:
                 args = cmdList[1]
-                if cmd == 'ID':
+                if cmd == "ID":
                     self._parse_id_received(args)
                     self._connectionStatus = API_AUTHENTICATED
-                elif cmd == 'CHN,1':
+                elif cmd == "CHN,1":
                     self._parse_change_received(args)
-                elif cmd == 'LIMITS':
+                elif cmd == "LIMITS":
                     self._parse_limits_received(args)
 
         await self._send_update_callback()
 
     def _parse_id_received(self, args):
         # ID:Model,MAC,IP,Protocol,Version,RSSI
-        info = args.split(',')
+        info = args.split(",")
         if len(info) >= 6:
             self._model = info[0]
             self._mac = info[1]
@@ -123,22 +130,22 @@ class IntesisBox(asyncio.Protocol):
             self._rssi = info[5]
 
     def _parse_change_received(self, args):
-        function = args.split(',')[0]
-        value = args.split(',')[1]
+        function = args.split(",")[0]
+        value = args.split(",")[1]
         if value == NULL_VALUE:
             value = None
         self._device[function] = value
 
     def _parse_limits_received(self, args):
-        split_args = args.split(',', 1)
+        split_args = args.split(",", 1)
 
         if len(split_args) == 2:
             function = split_args[0]
-            values = split_args[1][1:-1].split(',')
+            values = split_args[1][1:-1].split(",")
 
             if function == FUNCTION_SETPOINT and len(values) == 2:
-                self._setpoint_minimum = int(values[0])/10
-                self._setpoint_maximum = int(values[1])/10
+                self._setpoint_minimum = int(values[0]) / 10
+                self._setpoint_maximum = int(values[1]) / 10
             elif function == FUNCTION_FANSP:
                 self._fan_speed_list = values
             elif function == FUNCTION_MODE:
@@ -152,7 +159,7 @@ class IntesisBox(asyncio.Protocol):
     def connection_lost(self, exc):
         """asyncio callback for a lost TCP connection"""
         self._connectionStatus = API_DISCONNECTED
-        _LOGGER.info('The server closed the connection')
+        _LOGGER.info("The server closed the connection")
         self._send_update_callback()
 
     async def connect(self):
@@ -165,7 +172,8 @@ class IntesisBox(asyncio.Protocol):
             self._connectionStatus = API_CONNECTING
             if self._connectionRetries > 0:
                 _LOGGER.debug(
-                    "Couldn't connect to IntesisBox, retrying in %i minutes", self._connectionRetries
+                    "Couldn't connect to IntesisBox, retrying in %i minutes",
+                    self._connectionRetries,
                 )
                 await asyncio.sleep(self._connectionRetries * 60)
             try:
@@ -173,12 +181,13 @@ class IntesisBox(asyncio.Protocol):
                 self._reader, self._writer = await asyncio.open_connection(
                     self._ip, self._port
                 )
-                _LOGGER.debug('Opening connection to IntesisBox %s:%s',
-                            self._ip, self._port)
+                _LOGGER.debug(
+                    "Opening connection to IntesisBox %s:%s", self._ip, self._port
+                )
                 self._eventLoop.create_task(self._handle_packets())
 
             except Exception as ex:
-                _LOGGER.debug('%s Exception. %s / %s', type(ex), repr(ex.args), ex)
+                _LOGGER.debug("%s Exception. %s / %s", type(ex), repr(ex.args), ex)
                 self._connectionRetries += 1
                 self._connectionStatus = API_DISCONNECTED
                 # raise IHConnectionError from ex
@@ -193,13 +202,20 @@ class IntesisBox(asyncio.Protocol):
             self._reader._transport.close()
 
     async def poll_status(self, sendcallback=False):
-        self._writer.write("GET,1:*\r".encode('ascii'))
+        self._writer.write("GET,1:*\r".encode("ascii"))
         await self._writer.drain()
 
-        if len(self._operation_list) < 1: 
-            LIMITS = ["LIMITS:SETPTEMP","LIMITS:FANSP","LIMITS:MODE","LIMITS:VANEUD","LIMITS:VANELR","ID"]
+        if len(self._operation_list) < 1:
+            LIMITS = [
+                "LIMITS:SETPTEMP",
+                "LIMITS:FANSP",
+                "LIMITS:MODE",
+                "LIMITS:VANEUD",
+                "LIMITS:VANELR",
+                "ID",
+            ]
             for limit in LIMITS:
-                self._writer.write((limit + '\r').encode('ascii'))
+                self._writer.write((limit + "\r").encode("ascii"))
                 await self._writer.drain()
 
     async def set_temperature(self, setpoint):
@@ -223,11 +239,11 @@ class IntesisBox(asyncio.Protocol):
         """Internal method to send a command to the API"""
         message = "SET,{}:{},{}\r".format(1, uid, value)
         try:
-            self._writer.write(message.encode('ascii'))
+            self._writer.write(message.encode("ascii"))
             await self._writer.drain()
             _LOGGER.debug("Data sent: {!r}".format(message))
         except Exception as e:
-            _LOGGER.error('%s Exception. %s / %s', type(e), e.args, e)
+            _LOGGER.error("%s Exception. %s / %s", type(e), e.args, e)
 
     async def set_mode(self, mode):
         if not self.is_on:
@@ -362,9 +378,9 @@ class IntesisBox(asyncio.Protocol):
         """Send a keepalive command to reset it's watchdog timer."""
         await asyncio.sleep(10, loop=self._eventLoop)
 
-    @staticmethod 
+    @staticmethod
     def twos_complement_16bit(val):
         """Internal method to compute Two's Complement, to represent negative temperatures"""
         if (val & (1 << 15)) != 0:
             val = val - (1 << 16)
-        return val   
+        return val
